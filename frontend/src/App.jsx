c47a3dcc758eb678a33130e2fe2ebc813ce62188
raJiB <pokhrelrajib016@gmail.com>
2024-08-07 19:07:21 +0545

    project initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ForgotComponent from "./pages/forgot"
+import LoginComponetn from "./pages/login"
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
