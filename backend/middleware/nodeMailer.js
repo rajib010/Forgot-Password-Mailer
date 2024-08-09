cf53c2551327c6f1666a6b7daf5af08ee0b501d6
raJiB <pokhrelrajib016@gmail.com>
2024-08-09 17:04:30 +0545

    setup node mailer

---
+++ backend/middleware/nodeMailer.js
@@ -0,0 +1,35 @@
+import nodeMailer from "nodemailer"
+import crypto from "crypto"
+
+const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+const generateResetToken = function () {
+    return crypto.randomBytes(20).toString('hex');
+}
+
+const sendResetEmail = async function (userName, resetToken) {
+    const resetLink = `${process.env.DOMAIN}/reset-password?token=${resetToken}`;
+    const mailOptions = {
+        from: process.env.GMAIL,
+        to: userEmail,
+        subject: 'password reset',
+        text: `Please click on the link below to reset your password: ${resetLink}`
+    }
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Password reset link sent.');
+    } catch (error) {
+        console.log('Error in sending mail', error);
+
+    }
+}
+
+
+export default { transporter, sendResetEmail, generateResetToken }
